60659d585a29b1a80a1a5d33d185b49280e9feb0
TiagoDeveloping <55623870+TiagoDeveloping@users.noreply.github.com>
2019-11-24 17:20:32 +0100

    Global menu bar done

---
+++ site/assets/header.js
@@ -0,0 +1,6 @@
+function loadHeader() {
+    // Setup the header
+    var header = document.getElementById('header');
+    header.innerHTML = '<div id="topBeam"></div><div id="banner"><img src="assets/logo.jpg"/><h1>Custom Recipes</h1></div>';
+    console.log("header loaded");
+}
